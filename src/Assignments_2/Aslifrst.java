6a5a10f4a024fee8a687fa745cf1538502d7841c
Aslihb <aslihanbalci2209@icloud.com>
2022-04-22 18:58:14 -0400

    ikh

--- src/Assignments_2/Aslifrst.java
+++
@@ -1,8 +0,0 @@
-package Assignments_2;
-
-public class Aslifrst {
-    public static void main(String[] args) {
-        System.out.println("Merhaba");
-        System.out.println("guncelleme icin");
-    }
-}
--- src/Assignments_3/deneme.java
+++
@@ -1,7 +0,0 @@
-package Assignments_3;
-
-public class deneme {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
