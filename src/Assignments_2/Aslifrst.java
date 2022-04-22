60e7bce4dce99db8a311ad58cb8f22c6f9a144a3
Aslihb <aslihanbalci2209@icloud.com>
2022-04-22 18:56:55 -0400

    deneme

--- src/Assignments_2/Aslifrst.java
+++ src/Assignments_2/Aslifrst.java
@@ -3,5 +3,6 @@ package Assignments_2;
 public class Aslifrst {
     public static void main(String[] args) {
         System.out.println("Merhaba");
+        System.out.println("guncelleme icin");
     }
 }
